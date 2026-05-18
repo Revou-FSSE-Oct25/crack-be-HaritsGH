218d332dfda21b89b7ea86947dd2a74db4d32d63
Harits Ghiffari Hanif <harits.gh1@gmail.com>
2026-05-18 11:06:36 +0700

    edit: main.ts add * to route

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'https://crack-fe-harits-gh.vercel.app/',
+    origin: 'https://crack-fe-harits-gh.vercel.app/*',
     credentials: true
   });
 
