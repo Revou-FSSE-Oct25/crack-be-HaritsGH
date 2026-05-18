79434e2dfb83b3b298b0e3215063766d3a8a0d5f
Harits Ghiffari Hanif <harits.gh1@gmail.com>
2026-05-18 11:13:33 +0700

    edit: main.ts remove *

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'https://crack-fe-harits-gh.vercel.app*',
+    origin: '*',
     credentials: true
   });
 
